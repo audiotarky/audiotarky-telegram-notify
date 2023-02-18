75cb0f8c5c19f84ddfa3610ea092af783bce2e0a
Simon Metson <simon@metsonet.co.uk>
2023-02-18 10:51:02 +0000

    compress & exocde as hex the list of seen releases

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ import feedparser
 from mastodon import Mastodon
 from telegram import Bot
 import requests
+import zlib
+
+from argparse import ArgumentParser
 
 
 async def message(bot, message="hello world", channel=0):
@@ -38,6 +41,9 @@ def send_mastodon(the_message, creds):
 
 
 if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--dryrun", action="store_true")
+    args = parser.parse_args()
     time_fmt = "%a, %d %b %Y %H:%M:%S +0000"
     creds_file = Path("creds.json")
     creds = json.loads(creds_file.read_text())
@@ -45,9 +51,15 @@ if __name__ == "__main__":
     channel = int(creds["telegram"]["channel"])
     date = time.strptime(creds["last_update"], time_fmt)
     bot = Bot(token)
-    seen = creds.get("seen", [])
+    if "seen" in creds:
+        seen_bytes = bytes.fromhex(creds["seen"])
+        decomp = zlib.decompress(seen_bytes).decode()
+        seen = json.loads(decomp)
+    else:
+        seen = []
+
     print("fetching feed")
-    for item in reversed(feedparser.parse(creds["url"]).entries):
+    for item in reversed(feedparser.parse(creds["url"]).entries)):
         if item["link"] not in creds.get("seen", []):
             msg = re.sub(r"(.*from )(.*) \| (.*)", r"\1[\2](LINK) | \3", item["title"])
             msg = msg.replace("-", "\-")
@@ -59,18 +71,19 @@ if __name__ == "__main__":
             msg = msg.replace("|", "\|")
             print(item["title"])
             print(msg)
-            asyncio.run(message(bot, msg, channel))
-            plain_msg = f"{item['title']} - ({item['link']}?utm_campaign=new_release&utm_source=dischord)"
-            send_dischord(
-                plain_msg,
-                creds["dischord"]["token"],
-                creds["dischord"]["release_announce"],
-            )
-            plain_msg = f"{item['title']} - ({item['link']}?utm_campaign=new_release&utm_source=mastodon)"
-            send_mastodon(plain_msg, creds)
+            if not args.dryrun:
+                asyncio.run(message(bot, msg, channel))
+                plain_msg = f"{item['title']} - ({item['link']}?utm_campaign=new_release&utm_source=dischord)"
+                send_dischord(
+                    plain_msg,
+                    creds["dischord"]["token"],
+                    creds["dischord"]["release_announce"],
+                )
+                plain_msg = f"{item['title']} - ({item['link']}?utm_campaign=new_release&utm_source=mastodon)"
+                send_mastodon(plain_msg, creds)
             seen.append(item["link"])
 
     creds["last_update"] = time.strftime(time_fmt, time.localtime())
-    creds["seen"] = seen[-10:]
+    creds["seen"] = zlib.compress(json.dumps(seen[-60:]).encode("utf-8")).hex()
     with creds_file.open("w", encoding="UTF-8") as target:
-        json.dump(creds, target)
+        json.dump(creds, target, indent=4)
